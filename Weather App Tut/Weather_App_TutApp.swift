04954cb6693a1a4ba2bec019eb2870ebd63a8638
Ahmed Gagan <67264609+adgan11@users.noreply.github.com>
2023-08-19 12:53:29 +0530

    Initial Commit

---
+++ Weather App Tut/Weather_App_TutApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_App_TutApp.swift
+//  Weather App Tut
+//
+//  Created by Ahmed Gagan on 19/08/23.
+//
+
+import SwiftUI
+
+@main
+struct Weather_App_TutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
